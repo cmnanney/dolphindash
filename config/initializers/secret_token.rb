155ea47f0e913b825ef5494ece11e278072317d2
Christina Nanney <cmnanney@gmail.com>
2013-06-07 07:24:42 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dolphindash::Application.config.secret_token = 'b2e953c78e5a501a85ba79d5b04aad742c63e470c7e79cde000495a48b48527f4f45d15aea37a478bc991e155839259ea13d2066591bab44f76c59e40414466c'
